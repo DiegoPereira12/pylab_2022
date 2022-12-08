ace26169ace22542f468b0eebef14c655f73cf2a
unknown <diegoo.junior55@gmail.com>
2022-12-07 22:39:39 -0300

    update files 07122022

--- seletive/empresa/views.py
+++ seletive/empresa/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Tecnologias
+
 
 def nova_empresa(request):
-    return render(request, 'nova_empresa.html')
+    if request.method == "GET":
+        techs = Tecnologias.objects.all()
+        return render(request, 'nova_empresa.html', {'techs': techs})
+    elif request.method == "POST":
+        return HttpResponse('To aqui')
