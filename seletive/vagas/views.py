f8b50f2ae60952d3f6d63d458a6c34e58be1990e
unknown <diegoo.junior55@gmail.com>
2023-01-19 21:26:37 -0300

    update files 19012023

--- seletive/vagas/views.py
+++ seletive/vagas/views.py
@@ -1,36 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from empresa.models import Vagas
-from django.shortcuts import redirect
+from django.shortcuts import redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.messages import constants
+from .models import Tarefa
 
-def nova_vaga(request):
-    if request.method == "POST":
-        titulo = request.POST.get('titulo')
-        email = request.POST.get('email')
-        tecnologias_domina = request.POST.getlist('tecnologias_domina')
-        tecnologias_nao_domina = request.POST.getlist('tecnologias_nao_domina')
-        experiencia = request.POST.get('experiencia')
-        data_final = request.POST.get('data_final')
-        empresa = request.POST.get('empresa')
-        status = request.POST.get('status')
-
-        vaga = Vagas(
-            titulo=titulo,
-            email=email,
-            nivel_experiencia=experiencia,
-            data_final=data_final,
-            empresa_id=empresa,
-            status=status,
-        )
-
-        vaga.save()
-
-        vaga.tecnologias_estudar.add(*tecnologias_nao_domina)
-        vaga.tecnologias_dominadas.add(*tecnologias_domina)
-
-        vaga.save()
 
 def nova_vaga(request):
     if request.method == "POST":
@@ -65,6 +40,24 @@ def nova_vaga(request):
 
     elif request.method == "GET":
         raise Http404()
+    
+
+def vaga(request, id):
+    vaga = get_object_or_404(Vagas, id=id)
+    return render(request, 'vaga.html', {'vaga': vaga})
+
+def nova_tarefa(request, id_vaga):
+    titulo = request.POST.get('titulo')
+    prioridade = request.POST.get("prioridade")
+    data = request.POST.get('data')
+    
+    tarefa = Tarefa(vaga_id=id_vaga,
+                    titulo=titulo,
+                    prioridade=prioridade,
+                    data=data)
+    tarefa.save()
+    messages.add_message(request, constants.SUCCESS, 'Tarefa criada com sucesso')
+    return redirect(f'/vagas/vaga/{id_vaga}')
 
         
    
