3413ac5dc3b2fbce2ef739093da692900a4ffbac
unknown <diegoo.junior55@gmail.com>
2023-02-24 17:53:07 -0300

    update files 24022023

--- seletive/vagas/views.py
+++ seletive/vagas/views.py
@@ -4,8 +4,11 @@ from empresa.models import Vagas
 from django.shortcuts import redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.messages import constants
-from .models import Tarefa
-
+from .models import Tarefa, Emails
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from django.core.mail import EmailMultiAlternatives
+from django.conf import settings
 
 def nova_vaga(request):
     if request.method == "POST":
@@ -72,4 +75,19 @@ def realizar_tarefa(request, id):
     tarefa.save()    
     messages.add_message(request, constants.SUCCESS, 'Tarefa realizada com sucesso, parabéns!')
     return redirect(f'/vagas/vaga/{tarefa.vaga.id}')        
-   
+
+def envia_email(request, id_vaga):
+    vaga = Vagas.objects.get(id=id_vaga)
+    assunto = request.POST.get('assunto')
+    corpo = request.POST.get('corpo')
+
+    html_content = render_to_string('emails/template_email.html', {'corpo': corpo})
+    text_content = strip_tags(html_content)
+    email = EmailMultiAlternatives(assunto, text_content, settings.EMAIL_HOST_USER, [vaga.email,])
+    email.attach_alternative(html_content, "text/html")
+    if email.send():  
+        messages.add_message(request, constants.SUCCESS, 'Email enviado com sucesso.')
+        return redirect(f'/vagas/vaga/{id_vaga}')
+    else:
+        messages.add_message(request, constants.ERROR, 'Erro interno do sistema!')
+        return redirect(f'/vagas/vaga/{id_vaga}')
